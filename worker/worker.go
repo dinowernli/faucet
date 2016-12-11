439fb2c68aaef5ca313d3f5ab064afc7e9199fba
Dino Wernli <dinowernli@gmail.com>
2016-12-11 11:36:50 +0000

    Add a service for workers.

--- worker/worker.go
+++ worker/worker.go
@@ -5,9 +5,11 @@ import (
 
 	"dinowernli.me/faucet/demo"
 	pb_config "dinowernli.me/faucet/proto/config"
+	pb_worker "dinowernli.me/faucet/proto/service/worker"
 )
 
 func main() {
 	_ = &pb_config.Configuration{}
+	_ = &pb_worker.StatusRequest{}
 	fmt.Println(demo.Foo())
 }
