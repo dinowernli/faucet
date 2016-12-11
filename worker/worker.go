96e72278b7c68530763faf2b7a21cbade1a40d6b
Dino Wernli <dinowernli@gmail.com>
2016-12-11 11:49:38 +0000

    Start a grpc service for workers.

--- worker/worker.go
+++ worker/worker.go
@@ -1,15 +1,39 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net"
 
 	"dinowernli.me/faucet/demo"
 	pb_config "dinowernli.me/faucet/proto/config"
 	pb_worker "dinowernli.me/faucet/proto/service/worker"
+
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 )
 
+type workerService struct {
+}
+
+func (s *workerService) Status(context context.Context, request *pb_worker.StatusRequest) (*pb_worker.StatusResponse, error) {
+	return nil, grpc.Errorf(codes.Unimplemented, "Not implemented")
+}
+
 func main() {
+	log.Printf(demo.Foo())
+
 	_ = &pb_config.Configuration{}
 	_ = &pb_worker.StatusRequest{}
-	fmt.Println(demo.Foo())
+
+	server := grpc.NewServer()
+	pb_worker.RegisterWorkerServer(server, &workerService{})
+
+	listen, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	log.Printf("Starting server")
+	server.Serve(listen)
 }
