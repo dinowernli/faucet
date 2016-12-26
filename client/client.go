c7504f7ecae62f6fa33b0669a0d8889a10e6128b
Dino Wernli <dinowernli@users.noreply.github.com>
2016-12-26 23:53:12 +0200

    Add a slightly yolo client binary. (#11)
    * Add a slightly yolo client binary.
    
    * Add some helpful comments.

--- client/client.go
+++ client/client.go
@@ -1,20 +1,95 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"os"
+	"os/exec"
+	"path/filepath"
+	"regexp"
+	"strings"
 
 	pb_coordinator "dinowernli.me/faucet/proto/service/coordinator"
 
 	"github.com/Sirupsen/logrus"
 )
 
+const (
+	// TODO(dino): Check for the existence of the binaries, allow overriding.
+	bazelBinary = "bazel"
+	gitBinary   = "git"
+)
+
+var (
+	// TODO(dino): Make this more robust.
+	workspaceNameRegex = regexp.MustCompile("workspace\\(name = \"([a-z_]+)\"\\)")
+)
+
 func main() {
 	logger := logrus.New()
 	logger.Out = os.Stderr
 
-	_ = &pb_coordinator.CheckRequest{}
-	// TODO(dino): Take a few command line args to populate the request body as
-	// well as to find the coordinator, then send request to coordinator.
+	// TODO(dino): Check that there are no uncommited changes.
+	// TODO(dino): Have better messaging around common cases, e.g.,:
+	// - No workspace
+	// - Workspace without workspace_name declaration
 
-	logger.Infof("Hello world")
+	commitHash, err := getCommitHash()
+	if err != nil {
+		logger.Errorf("Unable to get commit hash: %v", err)
+		return
+	}
+
+	workspaceName, err := getWorkspaceName()
+	if err != nil {
+		logger.Errorf("Unable to get workspace name: %v", err)
+		return
+	}
+
+	request := &pb_coordinator.CheckRequest{
+		WorkspaceName: workspaceName,
+		GitCommit:     commitHash,
+	}
+	logger.Infof("Request: %v", request)
+}
+
+func getWorkspaceName() (string, error) {
+	// Find the workspace root.
+	workspacePath, err := getWorkspacePath()
+	if err != nil {
+		return "", fmt.Errorf("Unable to get workspace path: %v", err)
+	}
+
+	// Read the WORKSPACE file.
+	workspaceFilePath := filepath.Join(workspacePath, "WORKSPACE")
+	fileBytes, err := ioutil.ReadFile(workspaceFilePath)
+	if err != nil {
+		return "", fmt.Errorf("Unable to read WORKSPACE file [%s]: %v", workspaceFilePath, err)
+	}
+
+	// Find the workspace name inside it.
+	results := workspaceNameRegex.FindStringSubmatch(string(fileBytes))
+
+	// Expect the result to contain exactly two entries: one for the entire match and one for
+	// the the actual workspace name.
+	if len(results) != 2 {
+		return "", fmt.Errorf("Expected 2 matches, but got %d. Matches: %q", len(results), results)
+	}
+	return results[1], nil
+}
+
+func getWorkspacePath() (string, error) {
+	outBytes, err := exec.Command(bazelBinary, "info", "workspace").Output()
+	if err != nil {
+		return "", fmt.Errorf("Unable to get workspace path: %v", err)
+	}
+	return strings.Trim(string(outBytes), "\r\n"), nil
+}
+
+func getCommitHash() (string, error) {
+	outBytes, err := exec.Command(gitBinary, "rev-parse", "HEAD").Output()
+	if err != nil {
+		return "", fmt.Errorf("Unable to execute git: %v", err)
+	}
+	return strings.Trim(string(outBytes), "\r\n"), nil
 }
